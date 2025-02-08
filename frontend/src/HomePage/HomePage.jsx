0c18bb5d300903408650bff226aa7f4ef71b5f59
Michael Charles <michaelcharles10904@gmail.com>
2025-02-08 00:17:58 -0600

    added material tailwind

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
-
+import { Button } from "@material-tailwind/react";
 
 
 function HomePage() {
     return (
-        <div>
-            hello
-        </div>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <Button>Button</Button>
+        </>
+        
     )
 }
 
