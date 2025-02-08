71cc3b23a0bd6e8b1aca8c7d077d6a5ce30e076c
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2025-02-08 11:04:12 -0600

    [backend] added notes on routes to implement

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(express.json());
+
+// routes (in order of importance):
+// - business registration
+//     POST{name, email, category, desc, addr, profile picture(s)}
+//     /signup/business
+//     use firebase createUserWithEmailAndPassword func to handle the user's pass
+// - user resgistration
+//     POST{name, email, categories_of_interest}
+//     /signup/user
+//     use firebase createUserWithEmailAndPassword func to handle the user's pass
+// - user/business login
+//     POST{email,name} /login
+// - view all businesses, send relevant ones
+//     GET{lat, lon} businesses/
+//
+// time permitting
+// - user profile page
+// - user profile page
+//     GET /accountinfo
+// - business profile page
+
 
 app.use(cors({
     origin: 'http://localhost:5173',
