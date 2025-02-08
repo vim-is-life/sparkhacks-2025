c899304a86454421180e4bdf9d9835b637bc89eb
ebrizuela1 <106275585+ebrizuela1@users.noreply.github.com>
2025-02-08 11:05:08 -0600

    Implemented leaflet map and created spacing for map and Business Detail space

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import MyMapComponent from "../components/Map";
+import "../index.css"
 
 
 
@@ -12,8 +14,15 @@ function HomePage() {
             <Link to={'/signup'}>
                 <Button>Button</Button>
             </Link>
-            
-
+            <div className='container'>
+                <div className="map">
+                    <MyMapComponent  />
+                </div>
+                
+                <div className='bg-white side-view'>
+                    <h3>Some Text</h3>
+                </div>
+            </div>
         </>
         
     )
